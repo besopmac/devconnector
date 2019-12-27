4132a95315cfcdb411d15c6609415b399535fdfa
Bruno N. Campos <bruno@mambowifi.com>
2019-12-27 18:26:13 -0300

    User Registration

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
 const app = express();
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Init Middleware
+app.use(express.json({ extended: false }));
 
+app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
